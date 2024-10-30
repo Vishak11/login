1cc2159807b232ae51405f790aa61f6b4efb0e2e
Vishak11 <vishakvn007@gmail.com>
2024-10-30 21:43:01 +0530

    v1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+function App() {
+  const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (!username || !password) {
+            setMessage("Both fields are mandatory");
+        } else if (username === "user" && password === "password") {
+            setMessage("Welcome, user!");
+        } else {
+            setMessage("Invalid username or password");
+        }
+    };
+
+    return (
+        <div >
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit" >Submit</button>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
+
+
+
+
+export default App
